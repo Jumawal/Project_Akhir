4291b7cc2f79102b84413ef2c6442668b49fb378
jumawal <jumawalgbk@gmail.com>
2018-06-06 15:56:28 +0700

    tiang

--- src/org/yourorghere/GLRenderer.java
+++ src/org/yourorghere/GLRenderer.java
@@ -30,12 +30,29 @@ public class GLRenderer implements GLEventListener {
     vector lateral2 = new vector(1f, 0f, 0f);//deklarasi awal vektor untuk gerakan ke kanan
     vector vertical = new vector(0f, 1f, 0f);//deklarasi awal vetor untuk gerakan naik
     vector vertical2 = new vector(0f, -1f, 0f);//deklarasi awal vetor untuk gerakan naik
-/*
+    vector Sumbu_z = new vector(0f, 0f, -1f);//deklarasiawal vektor untuk maju & mundur 
+    vector Sumbu_x = new vector(1f, 0f, 0f);//deklarasi awal vektor untuk gerakan ke kanan & kiri 
+    vector Sumbu_y = new vector(0f, 1f, 0f);//deklarasi awal vetor untuk gerakan naik & turun
+
+    float Cx = 0, Cy = 2.5f, Cz = 0;
+    float Lx = 0, Ly = 2.5f, Lz = -20f;
+
+    float sudut_x = 0f;
+    float sudut_x2 = 0f;
+
+    float sudut_z = 0f;
+    float sudut_z2 = 0f;
+
+    float sudut_y = 0f;
+    float sudut_y2 = 0f;
+
+    boolean ori = true;
+
+    /*
      ini adalah metod untuk melakukan pergerakan.
      magnitude adalah besarnya gerakan sedangkan direction digunakan untuk menentukan arah.
      gunakan -1 untuk arah berlawanan dengan vektor awal
      */
-
     private void vectorMovement(vector toMove, float magnitude, float direction) {
         float speedX = toMove.x * magnitude * direction;
         float speedY = toMove.y * magnitude * direction;
@@ -47,18 +64,29 @@ public class GLRenderer implements GLEventListener {
         Ly += speedY;
         Lz += speedZ;
     }
-    float Cx = 5f, Cy = 0f, Cz = 5f;
-    float Lx = 0f, Ly = 0f, Lz = -20f;
+//    float Cx = 5f, Cy = 0f, Cz = 5f;
+//    float Lx = 0f, Ly = 0f, Lz = -20f;
 
     public void init(GLAutoDrawable drawable) {
-
         GL gl = drawable.getGL();
-        System.err.println("INIT GL IS: " + gl.getClass().getName());
-
+        System.err.println("INIT GL IS: "
+                + gl.getClass().getName());
+        // Enable VSync
         gl.setSwapInterval(1);
-
-        gl.glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
-        gl.glShadeModel(GL.GL_SMOOTH); // try setting this to GL_FLAT and see what happens.
+        float ambient[] = {1.0f, 1.0f, 1.0f, 1.0f};
+        float diffuse[] = {1.0f, 1.0f, 1.0f, 1.0f};
+        float position[] = {1.0f, 1.0f, 1.0f, 0.0f};
+        gl.glLightfv(GL.GL_LIGHT0, GL.GL_AMBIENT,
+                ambient, 0);
+        gl.glLightfv(GL.GL_LIGHT0, GL.GL_DIFFUSE,
+                diffuse, 0);
+        gl.glLightfv(GL.GL_LIGHT0, GL.GL_POSITION,
+                position, 0);
+        gl.glEnable(GL.GL_LIGHT0);
+        gl.glEnable(GL.GL_LIGHTING);
+        gl.glEnable(GL.GL_DEPTH_TEST);
+        gl.glClearColor(1f, 1f, 1.0f, 1.0f);
+        gl.glShadeModel(GL.GL_SMOOTH);
     }
 
     public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
@@ -79,16 +107,50 @@ public class GLRenderer implements GLEventListener {
     public void display(GLAutoDrawable drawable) {
         GL gl = drawable.getGL();
         GLU glu = new GLU();
+
         gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+
         gl.glLoadIdentity();
-        glu.gluLookAt(Cx, Cy, Cz,
-                Lx, Ly, Lz,
-                0, 1, 0);
+
+        glu.gluLookAt(Cx, Cy, Cz, Lx, Ly, Lz, Sumbu_y.x, Sumbu_y.y, Sumbu_y.z);
+
+        //untuk kotak
         gl.glPushMatrix();
-        gl.glTranslatef(3f, -1f, -5f);
-        gl.glRotatef(-90.0f, 1.0f, 0.0f, 0.0f);
-        gl.glRotatef(180.0f, 0.0f, 0.0f, 1.0f);
+        gl.glTranslatef(-1f, 0.5f, -15f);
+        gl.glRotatef(90f, 1f, 0, 0);
+        objek.kotak(gl);
+        gl.glPopMatrix();
+
+        //untuk tabung pertama
+        gl.glPushMatrix();
+        gl.glTranslatef(0.5f, 2f, -15f);
+        gl.glRotatef(90f, 1f, 0, 0);
         objek.Tabung(gl);
+        gl.glPopMatrix();
+
+        //untuk tabung kedua
+        gl.glPushMatrix();
+        gl.glTranslatef(0.5f, 4f, -15f);
+        gl.glRotatef(90f, 1f, 0, 0);
+        objek.Tabung2(gl);
+        gl.glPopMatrix();
+
+        //untuk kotak atas
+        gl.glPushMatrix();
+        gl.glTranslatef(0f, 4f, -15.2f);
+        gl.glRotatef(90f, 1f, 0, 0);
+        objek.kotak2(gl);
+        gl.glPopMatrix();
+
+//        //untuk baling baling
+//        gl.glPushMatrix();
+//        gl.glTranslatef(0.5f, 3.5f, -15.2f);
+//        gl.glRotatef(90f, 1f, 0, 0);
+//        objek.baling(gl);
+//        gl.glPopMatrix();
+
+        gl.glPushMatrix();
+        objek.BigBox(gl);
         gl.glPopMatrix();
 
         gl.glFlush();
@@ -130,8 +192,7 @@ public class GLRenderer implements GLEventListener {
             Lx = 0f;
             Ly = 0f;
             Lz = 0f;
-        } 
-        else {
+        } else {
             throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.     
         }
 
