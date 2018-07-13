acc38aca8ab8c2aaf4c34f225cd87e2a8a1119d0
jumawal <jumawalgbk@gmail.com>
2018-07-13 21:54:52 +0700

    upload

--- src/org/yourorghere/GLRenderer.java
+++ src/org/yourorghere/GLRenderer.java
@@ -4,6 +4,8 @@ package org.yourorghere;
  *
  * @author JUMAWAL_GaGaH
  */
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
 import javax.media.opengl.GL;
 import javax.media.opengl.GLAutoDrawable;
 import javax.media.opengl.GLEventListener;
@@ -73,20 +75,27 @@ public class GLRenderer implements GLEventListener {
                 + gl.getClass().getName());
         // Enable VSync
         gl.setSwapInterval(1);
-        float ambient[] = {1.0f, 1.0f, 1.0f, 1.0f};
-        float diffuse[] = {1.0f, 1.0f, 1.0f, 1.0f};
-        float position[] = {1.0f, 1.0f, 1.0f, 0.0f};
-        gl.glLightfv(GL.GL_LIGHT0, GL.GL_AMBIENT,
-                ambient, 0);
-        gl.glLightfv(GL.GL_LIGHT0, GL.GL_DIFFUSE,
-                diffuse, 0);
-        gl.glLightfv(GL.GL_LIGHT0, GL.GL_POSITION,
-                position, 0);
-        gl.glEnable(GL.GL_LIGHT0);
-        gl.glEnable(GL.GL_LIGHTING);
-        gl.glEnable(GL.GL_DEPTH_TEST);
-        gl.glClearColor(1f, 1f, 1.0f, 1.0f);
-        gl.glShadeModel(GL.GL_SMOOTH);
+        gl.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        gl.glShadeModel(GL.GL_SMOOTH); // try setting this to GL_FLAT and see what happens.
+        gl.glClearDepth(1.0f);
+        gl.glEnable(gl.GL_DEPTH_TEST);
+        gl.glDepthFunc(gl.GL_LEQUAL);
+        gl.glShadeModel(gl.GL_SMOOTH);
+        gl.glHint(gl.GL_PERSPECTIVE_CORRECTION_HINT, gl.GL_NICEST);
+//        float ambient[] = {1.0f, 1.0f, 1.0f, 1.0f};
+//        float diffuse[] = {1.0f, 1.0f, 1.0f, 1.0f};
+//        float position[] = {0.0f, 1.0f, 1.0f, 1.0f};
+////        gl.glLightfv(GL.GL_LIGHT0, GL.GL_AMBIENT,
+////                ambient, 0);
+////        gl.glLightfv(GL.GL_LIGHT0, GL.GL_DIFFUSE,
+////                diffuse, 1);
+//        gl.glLightfv(GL.GL_LIGHT0, GL.GL_POSITION,
+//                position, 1);
+//        gl.glEnable(GL.GL_LIGHT0);
+//        gl.glEnable(GL.GL_LIGHTING);
+//        gl.glEnable(GL.GL_DEPTH_TEST);
+//        gl.glClearColor(1f, 1f, 1.0f, 1.0f);
+//        //gl.glShadeModel(GL.GL_SMOOTH);
     }
 
     public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
@@ -103,6 +112,72 @@ public class GLRenderer implements GLEventListener {
         gl.glMatrixMode(GL.GL_MODELVIEW);
         gl.glLoadIdentity();
     }
+
+    static float deg2rad(double degree) {
+        return (float) (degree * (22.0 / 7) / 180);
+    }
+
+    static float getX(double sudut, double jarijari) {
+        return (float) (cos(deg2rad(sudut)) * jarijari);
+    }
+
+    static float getY(double sudut, double jarijari) {
+        return (float) (sin(deg2rad(sudut)) * jarijari);
+    }
+
+    static void segiLima(GL gl, float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4, float x5, float y5, float x6, float y6, float z) {
+        gl.glBegin(GL.GL_POLYGON);
+        gl.glVertex3f(x1, y1, z);
+        gl.glVertex3f(x2, y2, z);
+        gl.glVertex3f(x3, y3, z);
+        gl.glVertex3f(x4, y4, z);
+        gl.glVertex3f(x5, y5, z);
+        gl.glVertex3f(x6, y6, z);
+        gl.glEnd();
+    }
+
+    static void segiEmpatKecil(GL gl, float x1, float y1, float x2, float y2, float zDepan, float zBelakang) {
+        gl.glBegin(gl.GL_QUADS);
+        gl.glVertex3f(x1, y1, zDepan);
+        gl.glVertex3f(x2, y2, zDepan);
+        gl.glVertex3f(x2, y2, zBelakang);
+        gl.glVertex3f(x1, y1, zBelakang);
+        gl.glEnd();
+    }
+
+    static void kipas_angin(GL gl, float diameterluar, float diameterdalam, float tinggigigi, float ketebalan, float banyakgigi) {
+        float delta = (float) (360.0 / banyakgigi);
+        float sudut = 0;
+        float zdepan = (float) (1 * ketebalan);
+        float zbelakang = -zdepan;
+        for (int i = 0; i < banyakgigi; i++) {
+            float sudut1 = (float) (sudut - 2 * delta);
+            float sudut2 = (float) (sudut + 0.5 * delta);
+            float x1 = getX(sudut1, diameterdalam);
+            float y1 = getY(sudut1, diameterdalam);
+            float x2 = getX(sudut2, diameterdalam);
+            float y2 = getY(sudut2, diameterdalam);
+            float x3 = getX(sudut2, diameterluar);
+            float y3 = getY(sudut2, diameterluar);
+            float x4 = getX(sudut, diameterluar + tinggigigi);
+            float y4 = getY(sudut, diameterluar + tinggigigi);
+            float x5 = getX(sudut1, diameterluar);
+            float y5 = getY(sudut1, diameterluar);
+            float x6 = getX(sudut1, diameterluar);
+            float y6 = getY(sudut1, diameterluar);
+            gl.glColor3f(0, 1, 0);
+            segiLima(gl, x1, y1, x2, y2, x3, y3, x4, y4, x5, y5, x6, y6, zdepan);
+            segiLima(gl, x1, y1, x2, y2, x3, y3, x4, y4, x5, y5, x6, y6, zbelakang);
+            gl.glColor3f(0, 1, 0);
+            segiEmpatKecil(gl, x3, y3, x4, y4, zdepan, zbelakang);
+            segiEmpatKecil(gl, x5, y5, x4, y4, zdepan, zbelakang);
+            segiEmpatKecil(gl, x1, y1, x2, y2, zdepan, zbelakang);
+            sudut += delta;
+
+        }
+    }
+    float angle = 0;
+    float direction = 1.0f;
 
     public void display(GLAutoDrawable drawable) {
         GL gl = drawable.getGL();
@@ -142,69 +217,77 @@ public class GLRenderer implements GLEventListener {
         objek.kotak2(gl);
         gl.glPopMatrix();
 
+        gl.glPushMatrix();
+        gl.glTranslatef(0.5f, 3.5f, -13.7f);
+        gl.glRotatef(angle, 0f, 1f, 1f);
+        kipas_angin(gl, 1.3f, 0.2f, 1f, 0f, 4);
+//        angle += direction;
+//        if (angle >= 45 || angle <= -45) {
+//            direction = -direction;
+//        }
+        angle++;
+        gl.glPopMatrix();
+        
         //untuk tabung ke3
         gl.glPushMatrix();
         gl.glTranslatef(0.5f, 3.6f, -15.5f);
         gl.glRotatef(5f, 1.0f, 0.0f, 0.0f);
+        gl.glRotatef(-angle, 0f, 1f, 0f);
         objek.tabung3(gl);
+        angle += direction;
+        if (angle >= 45 || angle <= -45) {
+            direction = -direction;
+        }
         gl.glPopMatrix();
 
         //untuk kotak baling baling atas
-        gl.glPushMatrix();
-        gl.glTranslatef(0.9f, 4.3f, -13.7f);
-        gl.glRotatef(90f, 0.0f, 0.0f, 1.0f);
-        objek.kotakK1(gl);
-        gl.glPopMatrix();
-
-        //untuk kotak baling baling bawah
-        gl.glPushMatrix();
-        gl.glTranslatef(0.9f, 2.5f, -13.7f);
-        gl.glRotatef(90f, 0.0f, 0.0f, 1.0f);
-        objek.kotakK1(gl);
-        gl.glPopMatrix();
-        
-        //untuk kotak baling baling kanan
-        gl.glPushMatrix();
-        gl.glTranslatef(1.5f, 3.0f, -13.7f);
-        gl.glRotatef(0f, 0.0f, 0.0f, 1.0f);
-        objek.kotakK1(gl);
-        gl.glPopMatrix();
-        
-        //untuk kotak baling baling kiri
-        gl.glPushMatrix();
-        gl.glTranslatef(-0.5f, 3.0f, -13.7f);
-        gl.glRotatef(0f, 0.0f, 0.0f, 1.0f);
-        objek.kotakK1(gl);
-        gl.glPopMatrix();
-        
+//        //untuk kotak baling baling bawah
+//        gl.glPushMatrix();
+//        gl.glTranslatef(0.9f, 2.5f, -13.7f);
+//        gl.glRotatef(90f, 0.0f, 0.0f, 1.0f);
+//        objek.kotakK1(gl);
+//        gl.glPopMatrix();
+//
+//        //untuk kotak baling baling kanan
+//        gl.glPushMatrix();
+//        gl.glTranslatef(1.5f, 3.0f, -13.7f);
+//        gl.glRotatef(0f, 0.0f, 0.0f, 1.0f);
+//        objek.kotakK1(gl);
+//        gl.glPopMatrix();
+//
+//        //untuk kotak baling baling kiri
+//        gl.glPushMatrix();
+//        gl.glTranslatef(-0.5f, 3.0f, -13.7f);
+//        gl.glRotatef(0f, 0.0f, 0.0f, 1.0f);
+//        objek.kotakK1(gl);
+//        gl.glPopMatrix();
         //tombol nomer 0
-        gl.glPushMatrix();  
+        gl.glPushMatrix();
         gl.glTranslatef(-0.5f, 0.3f, -13.0f);
         gl.glRotatef(5f, 1.0f, 0.0f, 0.0f);
         objek.tombol(gl);
         gl.glPopMatrix();
-        
+
         //tombol nomer 1
         gl.glPushMatrix();
         gl.glTranslatef(0.0f, 0.3f, -13.0f);
         gl.glRotatef(5f, 1.0f, 0.0f, 0.0f);
         objek.tombol(gl);
         gl.glPopMatrix();
-        
+
         //tombol nomer 2
         gl.glPushMatrix();
         gl.glTranslatef(0.5f, 0.3f, -13.0f);
         gl.glRotatef(5f, 1.0f, 0.0f, 0.0f);
         objek.tombol(gl);
         gl.glPopMatrix();
-        
-         //tombol nomer 3
+
+        //tombol nomer 3
         gl.glPushMatrix();
         gl.glTranslatef(1.0f, 0.3f, -13.0f);
         gl.glRotatef(5f, 1.0f, 0.0f, 0.0f);
         objek.tombol(gl);
         gl.glPopMatrix();
-
 
         gl.glPushMatrix();
         objek.BigBox(gl);
